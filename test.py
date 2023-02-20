b2aa82fbfafff0498e0666c7d6799b1d125fe2ef
TechOnly <779222056@qq.com>
2023-02-20 12:03:28 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -3,11 +3,11 @@ from transformers import AutoTokenizer
 
 tokenizer = AutoTokenizer.from_pretrained("./saved_tokenizer/")
 
-prompt = "乔峰道："
+prompt = "段誉"
 inputs = tokenizer(prompt, return_tensors="tf")
 print(inputs)
 
-model = TFAutoModelForCausalLM.from_pretrained("./saved_model/")
+model = TFAutoModelForCausalLM.from_pretrained("./saved_model_finetuned/")
 
 outputs = model.generate(**inputs)
 print(tokenizer.batch_decode(outputs, skip_special_tokens=True))
