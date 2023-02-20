0c4cf54da7a69f6436e424a7586c25cff9919c56
TechOnly <779222056@qq.com>
2023-02-20 16:12:11 +0800

    Update test.py

--- test.py
+++ test.py
@@ -9,6 +9,6 @@ print(inputs)
 
 model = TFAutoModelForCausalLM.from_pretrained("./saved_model_finetuned/")
 
-outputs = model.generate(**inputs)
+outputs = model.generate(**inputs, max_length=30, top_k=2, do_sample=False)
 for one in tokenizer.batch_decode(outputs, skip_special_tokens=True):
     print(one)
