c48b6220be141520c30a04e063a6fe76749fb63a
TechOnly <779222056@qq.com>
2023-02-23 09:36:38 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,13 +1,15 @@
 from transformers import TFAutoModelForCausalLM
 from transformers import AutoTokenizer
 
-tokenizer = AutoTokenizer.from_pretrained("./saved_tokenizer/")
+checkpoint_local = "./saved_dir/"
+
+tokenizer = AutoTokenizer.from_pretrained(checkpoint_local)
 
 prompts = ["乔峰", "段誉", "虚竹"]
 inputs = tokenizer(prompts, return_tensors="tf")
 print(inputs)
 
-model = TFAutoModelForCausalLM.from_pretrained("./saved_model_finetuned/")
+model = TFAutoModelForCausalLM.from_pretrained(checkpoint_local)
 
 outputs = model.generate(**inputs, max_length=30, top_k=2, do_sample=False)
 for one in tokenizer.batch_decode(outputs, skip_special_tokens=True):
