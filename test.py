f4420f6b546f2711a26b03daf54b1440aea7e02d
TechOnly <779222056@qq.com>
2023-04-04 15:56:48 +0800

    Update test.py

--- test.py
+++ test.py
@@ -6,11 +6,11 @@ checkpoint_local = "./saved_dir/"
 tokenizer = AutoTokenizer.from_pretrained(checkpoint_local)
 
 prompts = ["乔峰", "段誉", "虚竹"]
-inputs = tokenizer(prompts, return_tensors="tf")
+inputs = tokenizer(prompts, return_tensors="tf", return_token_type_ids=False)
 print(inputs)
 
 model = TFAutoModelForCausalLM.from_pretrained(checkpoint_local)
 
-outputs = model.generate(**inputs, max_length=30, top_k=2, do_sample=False)
+outputs = model.generate(**inputs, max_length=30, num_beams=5, do_sample=False)
 for one in tokenizer.batch_decode(outputs, skip_special_tokens=True):
     print(one)
